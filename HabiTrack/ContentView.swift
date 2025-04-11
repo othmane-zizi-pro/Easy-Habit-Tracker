5517595e8b4c87ef7e26243e89451c8260386f55
Othmane Zizi <othmane.zizi.pro@gmail.com>
2025-04-11 15:09:27 -0400

    Initial Commit

---
+++ HabiTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HabiTrack
+//
+//  Created by Othmane Zizi on 2025-04-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HabiTrack/HabiTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabiTrackApp.swift
+//  HabiTrack
+//
+//  Created by Othmane Zizi on 2025-04-11.
+//
+
+import SwiftUI
+
+@main
+struct HabiTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
